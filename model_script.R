17743203bf773e6fcc2c2a51117ec032d6f8afa2
mloranty <mloranty@colgate.edu>
2016-12-01 11:00:26 -0500

    added model script

--- model_script.R
+++ model_script.R
@@ -23,19 +23,34 @@ load("C:/Users/mloranty/Google Drive/GIS_projects/boreal_snow_picker/monthly_swe
 vcf <- as.data.frame(getValues(vcf.2014.ease))
 colnames(vcf) <- 'vcf'
 vcf$cell.id <- 1:nrow(vcf)
+vcf <- na.omit(vcf)
 
 # extract GLC200 land cover- note these are only cells with at least 50% of dominant LC class
 glc <- as.data.frame(getValues(glc.50pct.ease))
 colnames(glc) <- 'lc'
 glc$cell.id <- 1:nrow(glc)
+glc <- na.omit(glc)
 
 #extract yearley swe/temp values, each layer is a column
 snow <- getValues(swe.cru)
 # convert to vector - data extracted by column, we checked
 snow <- as.data.frame(as.vector(snow))
 colnames(snow) <- 'swe'
-
-
-
+snow <- na.omit(snow)
 snow$year <- rep(1980:2014, each=nrow(glc))
 snow$cell.id <- rep(1:nrow(glc),35)
+
+#combine data frames for ensuing wizardry
+covar <- join(vcf,glc,type='inner',by='cell.id')
+all.dat <- join(covar,snow,type='inner',by='cell.id')
+
+#make unique ids for year and LC
+
+year <- data.frame(year=unique(all.dat$year),year.id=seq(1,length(unique(all.dat$year))))
+
+all.dat <- join(all.dat,year,type='left',by='year')
+
+lc <- data.frame(lc=unique(all.dat$lc),lc.id=seq(1,length(unique(all.dat$lc))))
+all.dat <- join(all.dat,lc,type='left',by='lc')
+
+
