f9c8f4c8cbc4f852feafab25c68e0675c8ff0170
kroppheather <kroppheather@gmail.com>
2017-12-04 13:46:42 -0500

    started looking at one year of data

--- swe_depletion/swe_depletion.R
+++ swe_depletion/swe_depletion.R
@@ -1,13 +1,11 @@
-##########################
-#
-# fit swe depletion curves 
-# to annual GlobSNOW data 
-# 
-#
-# MML 4/6/17
-##########################
 
-rm(list=ls())
-require(sp)
-require(raster)
-require(rgdal)
+
+
+dat.swe <- read.csv("c:\\Users\\hkropp\\Google Drive\\swe_test\\glc_SWE_M2BGS_ease2_2010.csv")
+
+
+depletion<- function(b,day,mid,M){
+	(M/(1+exp(b*(day-mid))))+2
+}
+
+plot(seq(50,100),depletion(1,seq(50,100),75,10), type="l")
