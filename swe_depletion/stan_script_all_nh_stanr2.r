c6b9c882896669ece025ca9836227af9edf205e9
kroppheather <kroppheather@gmail.com>
2017-12-06 11:31:14 -0500

    data subset mistake

--- swe_depletion/stan_script_all_nh_stanr2.r
+++ swe_depletion/stan_script_all_nh_stanr2.r
@@ -89,12 +89,13 @@ inits1<-list(list(M=c(50),base=c(1),b=c(.1),
 			sig_swe=c(20)))
 			
 			
-for(i in 1:dim(gcUse)[1]){			
+for(i in 1:dim(gcUse)[1]){		
+		datalist <- list(list(Nobs=dim(sweDF3[sweDF3$gcID==i,])[1], swe=sweDF3$swe[sweDF3$gcID==i], 
+				day=sweDF3$doy[sweDF3$gcID==i]-(61+((152-61)/2))))
 	print(paste("start model run", i))			
 	if(rn==1){		
 	stan_model1 = stan("/home/hkropp/github/boreal_lw/swe_depletion/boreal_stan_nh_split.stan", 
-					data = list(Nobs=dim(sweDF3[sweDF3$gcID==i])[1], swe=sweDF3$swe[sweDF3$gcID==i], 
-				day=sweDF3$doy[sweDF3$gcID==i]-(61+((152-61)/2))),init=inits1,
+					data = datalist,init=inits1,
 				,chains=1, iter=3000)	
 	print(paste("end model run",i))	
 	out1<- extract(stan_model1)
@@ -108,8 +109,7 @@ for(i in 1:dim(gcUse)[1]){
 	}
 	if(rn==2){			
 	stan_model2 = stan("/home/hkropp/github/boreal_lw/swe_depletion/boreal_stan_nh_split.stan", 
-					data = list(Nobs=dim(sweDF3[sweDF3$gcID==i])[1], swe=sweDF3$swe[sweDF3$gcID==i], 
-				day=sweDF3$doy[sweDF3$gcID==i]-(61+((152-61)/2))),init=inits2,
+					data = datalist,init=inits2,
 				,chains=1, iter=3000)	
 	print(paste("end model run",i))
 	out2<- extract(stan_model2)	
@@ -124,8 +124,7 @@ for(i in 1:dim(gcUse)[1]){
 
 	if(rn==3){	
 	stan_model3 = stan("/home/hkropp/github/boreal_lw/swe_depletion/boreal_stan_nh_split.stan", 
-					data = list(Nobs=dim(sweDF3[sweDF3$gcID==i])[1], swe=sweDF3$swe[sweDF3$gcID==i], 
-				day=sweDF3$doy[sweDF3$gcID==i]-(61+((152-61)/2))),init=inits3,
+					data = datalist,init=inits3,
 				,chains=1, iter=3000)	
 	print(paste("end model run",i))
 	out3<- extract(stan_model3)	
