cd8ebc69d7bd3374bfc9fca128d31cd02d6cdb66
kroppheather <kroppheather@gmail.com>
2018-05-18 10:26:02 -0400

    finished working on figures

--- swe_model/swe_depletion_figure.r
+++ swe_model/swe_depletion_figure.r
@@ -19,7 +19,7 @@ plotDI <- "z:\\Projects\\boreal_swe_depletion\\figures\\model\\run2"
 ###  read in swe data  and organize      ###
 ############################################
 dat.swe <- read.csv("z:\\Projects\\boreal_swe_depletion\\data\\swe_depletion_model_data.csv")
-dat.glc <- read.csv("z:\\Projects\\boreal_swe_depletion\\data\\swe_depletion_model_data.csv")
+dat.glc <- read.csv("z:\\Projects\\boreal_swe_depletion\\data\\glc50_table.csv")
 
 #calculate proportion of land cover
 dat.swe$glc1.p <- dat.swe$glc1f/3136
@@ -139,6 +139,10 @@ chain3all <- as.mcmc(chain3all)
 codaAll <- mcmc.list(chain1all,chain2all,chain3all)
 
 modSum <- summary(codaAll)
+datC <- data.frame(cbind(modSum$statistics,modSum$quantiles))
+datC$glcID <- rep(seq(1,6),times=5)
+datC$parms <- gsub("\\d","",rownames(datC))
+
 ############################################
 ###  check history                       ###
 ############################################
@@ -149,5 +153,50 @@ mcmcplot(codaAll,dir=paste0(plotDI,"\\history") )
 ############################################
 ###  plot params                         ###
 ############################################
+#make a simplier glc plotting name
+IDSglc$name <- c("needleleaf deciduous","deciduous shrub","evergreen shrub","herbaceous",
+					"needleleaf evergreen","mixed tree")
+					
+#normalized day
+dat.swe4$dayN <- (dat.swe4$jday-32)/(182-32)
+#swe curve function
+sweC <- function(M, b,day,mid,base){
+	M/(1+exp(b*(day-mid)))+base
+}
+#plotting info					
+wd <- 50
+hd <- 50
 
+xl <- 0
+xh <- 1
+yl <- 0
+yh <- 0.5
+xplot <- seq(0,1,by=.01)
+
+xL <- seq(32,182, by=10)
+xS <- seq(0,1, length.outlength(xL))
+yS <- seq(0,0.5,by=.05)
+
+for(i in 1:dim(IDSglc)[1]){
+	jpeg(paste0(plotDI,"\\curve_",IDSglc$name[i],".jpeg"), width=2000,height=2000,quality=100)
+	layout(matrix(c(1),ncol=1),width=lcm(wd),height=lcm(hd))
+	par(mai=c(0,0,0,0))
+		plot(c(0,1),c(0,1), type="n", xlim=c(xl,xh), ylim=c(yl,yh), axes=FALSE,
+				xaxs="i", yaxs="i", xlab=" ", ylab=" ")
+				
+		points(dat.swe4$dayN,dat.swe4$swe,pch=19, col="grey75")
+
+		points(xplot, sweC(datC$Mean[datC$parms=="M"][i],
+							datC$Mean[datC$parms=="b"][i],
+							xplot,
+							datC$Mean[datC$parms=="mid"][i],
+							datC$Mean[datC$parms=="base"][i]),type="l", lwd=6,
+							col="tomato3")
+		axis(1, xS,rep("", length(xS)), lwd.ticks=5)
+		mtext(xL,at=xS, side=1, line=5, cex=2)
+		axis(2,yS, rep("", length(yS)), lwd.ticks=5)
+		mtext(yS,at=yS,side=2, line=5, cex=2, las=2)
+	dev.off()
+}					
+					
 
