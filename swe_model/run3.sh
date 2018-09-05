8883e8dfe07ce4158019c2ae50dd05837116d710
kroppheather <kroppheather@gmail.com>
2018-09-05 14:47:45 -0400

    finished setting up scripts to run on cluster

---
+++ swe_model/run3.sh
@@ -0,0 +1,17 @@
+
+#set number of nodes and processes
+PBS -l nodes=1 ppn=30
+#set max wall clock time
+PBS -l walltime=336:00:00
+#set name of the job
+PBS -N run3
+#mail alert at the start end and abortion of execution
+PBS -m bea
+#send mail to this address
+PBS -M hkropp@colgate.edu
+#use submission environment
+PBS -V
+#start job from the directory it was submitted
+cd $PBS_0_WORKDIR
+#run the script
+/shared/R-3.4.3/bin/R Rscript /home/hkropp/github/boreal_lw/swe_model/swe_depletion_model_script_r3.r
