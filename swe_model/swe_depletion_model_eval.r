4fee92e3390643a38183be9e42e98e95952a56ab
kroppheather <kroppheather@gmail.com>
2018-11-27 15:27:23 -0500

    started to add model checks

--- swe_model/swe_depletion_model_eval.r
+++ swe_model/swe_depletion_model_eval.r
@@ -20,32 +20,23 @@ DDdir <- c("/home/hkropp/boreal/data",
 # file name information                               #
 #######################################################
 
-#create a vector with all of the parent directories
-dirP <- c("z:\\projects\\boreal_swe_depletion\\model\\run9\\run1\\run1",
-			"z:\\projects\\boreal_swe_depletion\\model\\run9\\run2\\run2",
-			"z:\\projects\\boreal_swe_depletion\\model\\run9\\run3\\run3",
-			"z:\\projects\\boreal_swe_depletion\\model\\run9\\run4\\run4",
-			"z:\\projects\\boreal_swe_depletion\\model\\run9\\run5\\run5",
-			"z:\\projects\\boreal_swe_depletion\\model\\run9\\run6\\run6")
+#read in rep information
 
-outD <- "z:\\projects\\boreal_swe_depletion\\model\\run9\\eval"
+
+#create a vector with all of the parent directories
+dirP <- c("z:\\projects\\boreal_swe_depletion\\model\\run10\\run1")
+
+outD <- "z:\\projects\\boreal_swe_depletion\\model\\run10\\eval"
 #get a list of all of the files
 
-dirA <- list()
-dimA <- numeric(0)
-datA <- list()
-for(i in 1:length(dirP)){
-	dirA[[i]] <- list.dirs(paste0(dirP[i]), full.names=FALSE)
-	#remove parent directory
-	dirA[[i]] <- dirA[[i]][-1]
-	dimA[i] <- length(dirA[[i]])
-	datA[[i]] <- data.frame(files=dirA[[i]],dirP=rep(dirP[i],dimA[i]))
-	
-	
-}
 
-datA <- ldply(datA,data.frame)
-#create a 
+	dirA <- list.dirs(paste0(dirP), full.names=FALSE)
+	#remove parent directory
+	dirA <- dirA[-1]
+	dimA <- length(dirA)
+	datA <- data.frame(files=dirA,dirP=rep(dirP,dimA))
+
+
 
 #pull out identifying info
 
@@ -100,8 +91,8 @@ for(i in 1:dim(chainDF)[1]){
 	b0Out3 <- as.mcmc(b0Out3)
 	b0mcmc <- mcmc.list(b0Out1,b0Out2,b0Out3)
 	#make plots
-	#dir.create(paste0(outD,"\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
-	#mcmcplot(b0mcmc,dir=paste0(outD,"\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
+	dir.create(paste0(outD,"\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
+	mcmcplot(b0mcmc,dir=paste0(outD,"\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
 	b0.diag[[i]] <- gelman.diag(b0mcmc)
 }
 	
@@ -125,9 +116,180 @@ for(i in 1:dim(chainDF)[1]){
 	midOut3 <- as.mcmc(midOut3)
 	midmcmc <- mcmc.list(midOut1,midOut2,midOut3)
 	#make plots
-	#dir.create(paste0(outD,"\\mid\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
-	#mcmcplot(midmcmc,dir=paste0(outD,"\\mid\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
+	dir.create(paste0(outD,"\\mid\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
+	mcmcplot(midmcmc,dir=paste0(outD,"\\mid\\glc",chainDF$glc[i],"_year",chainDF$year[i]))
 	mid.diag[[i]] <- gelman.diag(midmcmc)
 }
 
+#create a dataframe indicating the convergence of each gridcell and glc
+midCheck <- list()
+b0Check <- list()
+for(i in 1:dim(chainDF)[1]){
 
+		midCheck[[i]] <- data.frame(conv=ifelse(mid.diag[[i]]$psrf[,1]>1.2,1,0),
+						gridID=seq(1,dim(mid.diag[[i]]$psrf)[1]),
+						glc=rep(chainDF$glc[i],dim(mid.diag[[i]]$psrf)[1]),
+						year=rep(chainDF$year[i],dim(mid.diag[[i]]$psrf)[1]))
+						
+		b0Check[[i]] <- data.frame(conv=ifelse(b0.diag[[i]]$psrf[,1]>1.2,1,0),
+						gridID=seq(1,dim(b0.diag[[i]]$psrf)[1]),
+						glc=rep(chainDF$glc[i],dim(b0.diag[[i]]$psrf)[1]),
+						year=rep(chainDF$year[i],dim(b0.diag[[i]]$psrf)[1]))
+}
+#
+
+
+#######################################################
+# read in and filter data                             #
+#######################################################
+#read in data files
+if(runOS==1){
+	dat.swe <- read.csv(paste0(DDdir[1], "/swe_depletion_model_data_vcf_no_topo.csv"))
+	dat.glc <- read.csv(paste0(DDdir[1], "/glc50_table.csv"))
+	whichrep <- read.csv(paste0(DDdir[1],"/rep_subID.csv"))
+
+}else{
+
+	dat.swe <- read.csv(paste0(DDdir[2],"\\swe_depletion_model_data_vcf_no_topo.csv"))
+	dat.glc <- read.csv(paste0(DDdir[2], "/glc50_table.csv"))
+	whichrep <- read.csv(paste0(DDdir[1],"\\rep_subID.csv"))
+}
+
+
+print("finish reading in data")
+
+##########################
+##### Subset point 1 #####
+##########################
+#only focus on 2000-2009 for now
+dat.swe <- dat.swe[dat.swe$year<=2009&dat.swe$year>=2000,]
+
+
+
+
+#calculate proportion of land cover
+dat.swe$glc1.p <- dat.swe$glc1f/3136
+hist(dat.swe$glc1.p )
+dat.swe$glc2.p <- dat.swe$glc2f/3136
+hist(dat.swe$glc2.p )
+
+#just use first glc class 				
+dat.swe$zone <- dat.swe$glc1
+
+
+##########################
+##### Filter point 1 #####
+##########################
+#start with the simplest classification of glc 
+#>50% is dominated by the landcover class
+dat.swe1 <- dat.swe[dat.swe$glc1.p>=.5,]
+
+
+##########################
+##### Filter point 2 #####
+##########################
+#filter out land cover types not of interest
+#don't include glc 3,9,10,14,15,16,18,19,20,21,22
+dat.swe2 <- dat.swe1[dat.swe1$zone!=2&dat.swe1$zone!=11&dat.swe1$zone!=3&dat.swe1$zone!=15&dat.swe1$zone!=16&dat.swe1$zone!=10&dat.swe1$zone!=14&
+				dat.swe1$zone!=9&dat.swe1$zone<18,]
+
+##########################
+##### Filter point 3 #####
+##########################				
+				
+#filter out areas with no snow extent in spring
+sweMax <- aggregate(dat.swe2$swe,by=list(dat.swe2$cell,dat.swe2$year),FUN="max")
+colnames(sweMax) <- c("cell","year","sweMax")
+sweMin <- aggregate(dat.swe2$swe,by=list(dat.swe2$cell,dat.swe2$year),FUN="min")
+colnames(sweMin) <- c("cell","year","sweMin")
+sweMax$Diff <- sweMax$sweMax-sweMin$sweMin
+te <- hist(sweMin$sweMin, breaks=seq(0,0.5, by=0.01))
+te2 <- hist(sweMax$Diff, breaks=seq(0,0.55, by=0.01))
+te3 <- hist(sweMax$sweMax, breaks=seq(0,0.6, by=0.01))
+#vast majority of minimum swe 97% is below 0.02 in a year
+#exclude any sites and years where the maximum swe does not exceed 0.04 in the year
+sweMaxF <- sweMax[sweMax$sweMax >=0.01,]
+#join back to swe to filter
+dat.swe3 <- join(dat.swe2, sweMaxF, by=c("cell","year"), type="inner")
+
+
+#######################################################
+# organize data for model run                         #
+#######################################################
+#create a table of identifiers
+#unique glc
+
+IDSglc <- unique(data.frame(zone=dat.swe3$zone))
+IDSglc <- join(IDSglc, dat.glc[,1:2], by="zone", type="left")
+IDSglc$gcID <- seq(1,dim(IDSglc)[1])
+
+
+#join glc ID into dataframe
+dat.swe4 <- join(dat.swe3,IDSglc, by="zone", type="left")
+
+#normalize swe
+#calculate percent
+dat.swe4$sweP <- dat.swe4$swe/dat.swe4$sweMax
+
+#round swe for 20% of peak to 1 and 20% of low to zero
+dat.swe4$sweN <- ifelse(dat.swe4$sweP>=0.8,1,
+					ifelse(dat.swe4$sweP<=0.2,0,(dat.swe4$sweP-0.2)/0.6))
+							
+					
+#get unique pixel id in each glc for parameter id
+
+pixID <- unique(data.frame(cell=dat.swe4$cell,year=dat.swe4$year, gcID=dat.swe4$gcID))
+
+#want each cell in each year, gcID that will be the subset to run each model
+#create year x gcID dataframe
+gcYearID <- unique(data.frame(year=dat.swe4$year, gcID=dat.swe4$gcID))
+
+#organize gcYear by the count of cells 
+pixCountList <- list()
+pixGLCTemp <- numeric(0)
+for(i in 1:dim(gcYearID)[1]){
+	pixCountList[[i]] <- pixID[pixID$gcID==gcYearID$gcID[i]&pixID$year==gcYearID$year[i],]
+	pixGLCTemp[i] <- dim(pixCountList[[i]])[1]
+}
+
+gcYearID$cellCount <- pixGLCTemp
+
+#organize gcYearID by cell count
+gcYearID <- gcYearID[order(gcYearID$cellCount),]
+
+gcYearID$gcYearID <- seq(1,dim(gcYearID)[1])
+
+#subset into each glc xYear
+pixList <- list()
+pixGLC <- numeric(0)
+for(i in 1:dim(gcYearID)[1]){
+	pixList[[i]] <- pixID[pixID$gcID==gcYearID$gcID[i]&pixID$year==gcYearID$year[i],]
+	pixList[[i]]$pixID <- seq(1,dim(pixList[[i]])[1])
+	pixList[[i]]$gcYearID <- rep(gcYearID$gcYearID[i], dim(pixList[[i]])[1])
+	pixGLC[i] <- dim(pixList[[i]])[1]
+}
+
+pixJ <- ldply(pixList,data.frame)
+
+#join back into swe
+dat.swe5 <- join(dat.swe4,pixJ, by=c("cell","year","gcID"), type="left")
+
+
+print("finish data organize")
+
+
+#pull out which rows each gc is related
+#sweRows <- list()
+#for(i in 1:dim(gcYearID)[1]){
+#	sweRows[[i]] <- which(dat.swe5$gcID==gcYearID$gcID[i]&dat.swe5$year==gcYearID$year[i])
+
+#}
+#find out which 
+#whichrep <- list()
+#for(i in 1:dim(gcYearID)[1]){
+#	whichrep[[i]] <- data.frame(gcID=rep(gcYearID$gcID[i], each=5000),year=rep(gcYearID$year[i], each=5000), rows=sample(sweRows[[i]],5000))
+#}
+#whichrep <- ldply(whichrep,data.frame)
+#write.table(whichrep,"z:\\projects\\boreal_swe_depletion\\data\\rep_subID.csv",sep=",",row.names=FALSE)
+
+datRep <- dat.swe5[whichrep$rows,]
