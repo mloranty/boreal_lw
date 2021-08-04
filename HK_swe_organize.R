89cb9525827c1c48cd7def190541b5cc0ae30f18
Heather Kropp <kroppheather@gmail.com>
2021-08-04 11:37:43 -0400

    updated for vcf in final output

--- HK_swe_organize.R
+++ HK_swe_organize.R
@@ -235,7 +235,7 @@ tm_shape(glc.maj2)+
                      "13: Herbaceous Cover, closed-open"))+
   tm_layout(legend.outside = TRUE, title = "Majority LC no mnts")
 
-pr.m2 <- mask(pr.m, glcP.mask,maskvalue=NA)
+pr.m2 <- mask(pr.m, glc.maj2,maskvalue=NA)
 plot(pr.m2)
 
 
@@ -605,8 +605,8 @@ dataAllFinal1 <- list()
 dataAllFinal2 <- list()
 YearDF <- list()
 for(i in 1:NYears){
-  dataAll[[i]] <- stack(melt.mm.day[[i]],glc2000,doyStart[[i]],maxSwe[[i]],meltMeanT[[i]])
-  names(dataAll[[i]]) <- c("melt.mm.day","glc","doyStart","maxSwe.m","meltTempC")
+  dataAll[[i]] <- stack(melt.mm.day[[i]],glc2000,vcf.mask,doyStart[[i]],maxSwe[[i]],meltMeanT[[i]])
+  names(dataAll[[i]]) <- c("melt.mm.day","glc","vcf","doyStart","maxSwe.m","meltTempC")
   dataDF[[i]] <-  getValues(dataAll[[i]])
   YearDF[[i]] <- data.frame(year=rep(Years[i], nrow(dataDF[[i]])))
   dataAllFinal1[[i]] <- cbind(dataDF[[i]],LatLong)
@@ -630,6 +630,7 @@ rm(list=setdiff(ls(), c("dailySwe",
                         "glcID",
                         "maxSwe",
                         "meltMeanT",
+                        "vcf.mask",
                         "analysisDF")))
 
 
