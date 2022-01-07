eee36f88d90b495d1dadc218bee82659f6d7a569
Heather Kropp <kroppheather@gmail.com>
2022-01-07 09:05:08 -0500

    restored old analysis files
    SWE max shows same patterns as unnormalized and is harder to interpret. Did not follow the predictions of the group. Sticking with original unnormalized measure of swe

--- HK_model_code.R
+++ HK_model_code.R
@@ -8,7 +8,7 @@ model{
     rep.b0[i] ~ dnorm(mu.b0[i],tau.b0[glcIDB[i]])
     #empirical regression
     mu.b0[i] <- betaB0[glcIDB[i]] + betaB1[glcIDB[i]]*TempAB[i] + betaB2[glcIDB[i]]*(CanopyB[i]-20) +
-      betaB3[glcIDB[i]]*(sweDay[i]-107)  +
+      betaB3[glcIDB[i]]*(sweDay[i]-107) + betaB4[glcIDB[i]]*(SweMax[i]-log(0.15))  +
       eps.b[GCyearB[i]]
 
     
@@ -42,7 +42,7 @@ model{
     betaB1[i] ~ dnorm(mu.betaB1,tau.betaB1)
     betaB2[i] ~ dnorm(mu.betaB2,tau.betaB2)
     betaB3[i] ~ dnorm(mu.betaB3,tau.betaB3)
-
+    betaB4[i] ~ dnorm(mu.betaB4,tau.betaB4)
     
     #likelihood standard deviation
     tau.b0[i] <- pow(sig.b0[i],-2)
@@ -65,19 +65,20 @@ model{
   mu.betaB1 ~ dnorm(0,0.00001)
   mu.betaB2 ~ dnorm(0,0.00001)
   mu.betaB3 ~ dnorm(0,0.00001)
-
+  mu.betaB4 ~ dnorm(0,0.00001)
   
   tau.betaB0 <- pow(sig.B0, -2)
   tau.betaB1 <- pow(sig.B1, -2)
   tau.betaB2 <- pow(sig.B2, -2)
   tau.betaB3 <- pow(sig.B3, -2)
-
+  tau.betaB4 <- pow(sig.B4, -2)
+  
   
   sig.B0 ~ dunif(0,1000)
   sig.B1 ~ dunif(0,1000)
   sig.B2 ~ dunif(0,1000)
   sig.B3 ~ dunif(0,1000)
-
+  sig.B4 ~ dunif(0,1000)
 
   ####################
   #####regression#####
@@ -89,7 +90,7 @@ model{
       mu.Temp[j,i] <- betaB0S[i] + betaB1[i]*TempMean[j] 
       mu.Canopy[j,i] <- betaB0S[i] + betaB2[i]*(CanopyMean[j]-20)
       mu.Onset[j,i] <- betaB0S[i] + betaB3[i]*(SdayMean[j]-107)
-    
+      mu.Max[j,i] <- betaB0S[i] + betaB4[i]*(MaxMean[j]-log(0.15))
     }
   }	
   
